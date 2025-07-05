9ddc8c0a7b8487ff2cee19b3e71cba701bdaea65
KhushiRaj23 <khushiraj23k3@gmail.com>
2025-07-05 09:37:33 +0530

    updated UserControllerTest (testDeleteUserFound, testDeleteUserNotFound)

--- src/test/java/com/user/usermanagementapi/UserControllerTest.java
+++ src/test/java/com/user/usermanagementapi/UserControllerTest.java
@@ -98,8 +98,9 @@ public class UserControllerTest {
 
     @Test
     void testDeleteUserNotFound() throws Exception{
-        Long nonExistedId=user1.getId()+101;  //any value which not exists in database
+        Long nonExistedId=user1.getId()+101;  //any value which does not exist in database
         mockMvc.perform(delete("/api/users/{id}",nonExistedId)).andExpect(status().isNotFound());
         assertEquals(initialUserCount,userRepository.count());
     }
+
 }
