830942d1ab49e9a7773d05896e2c961afcc3be61
KhushiRaj23 <khushiraj23k3@gmail.com>
2025-07-06 15:49:53 +0530

    updated User Class Used Lombok for Getter-Sectter-Constructors

--- src/main/java/com/user/usermanagementapi/model/User.java
+++ src/main/java/com/user/usermanagementapi/model/User.java
@@ -5,24 +5,34 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+
+
 //marks this class as a JPA entity
 @Entity
 @Table(name="users")
+//@Getter  //THis annotation is used to make Getter function automatically
+//@Setter  //This annotation is used to make Setter function automatically
+@Data     //This annotation is used to make Getter + Setter + NoArgsConstructor + AllArgsConstructor all together at compilation
+@NoArgsConstructor       //This annotation is used for default constructor
+@RequiredArgsConstructor  //This annotation is used for nonNull field Constructor
 public class User {
 
     @Id  //specifies the PRIMARY KEY of the entity
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     @NotBlank(message = "Name is required")
     @Size(min = 2,max = 100,message = "Name must be between 2 and 100 characters")
     private String name;
 
+    @NonNull
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
@@ -35,52 +45,5 @@ public class User {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-    public User(){}
-    public User(String name,String email){
-        this.name=name;
-        this.email=email;
 
-    }
-    public User(Long id,String name,String email,LocalDateTime createdAt,LocalDateTime updatedAt){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.createdAt=createdAt;
-        this.updatedAt=updatedAt;
-    }
-
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt(){
-        return updatedAt;
-    }
-    public void setUpdatedAt(LocalDateTime updatedAt){
-        this.updatedAt=updatedAt;
-    }
 }
